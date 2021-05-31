f4c925a220896f4a0a07282ef70b2d6ac0a08362
Thamer Saraei <tsarei@legroupejsv.onmicrosoft.com>
2021-05-30 22:42:35 -0400

    ADD: Animate

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ container.addEventListener("mouseenter", (e) => {
   card.style.transition = "none";
   //Popout
   title.style.transform = "translateZ(150px)";
-  sneaker.style.transform = "translateZ(200px) rotateZ(-45deg)";
+  sneaker.style.transform = "translateZ(200px) rotateZ(-30deg)";
   description.style.transform = "translateZ(125px)";
   price.style.transform = "translateZ(125px)"
   sizes.style.transform = "translateZ(100px)";
@@ -40,4 +40,47 @@ container.addEventListener("mouseleave", (e) => {
   sizes.style.transform = "translateZ(0px)";
   purchase.style.transform = "translateZ(0px)";
   cart.style.transform = 'translateZ(0px)';
+});
+
+
+// Some random colors
+const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
+
+const numBalls = 80;
+const balls = [];
+
+for (let i = 0; i < numBalls; i++) {
+  let ball = document.createElement("div");
+  ball.classList.add("ball");
+  ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+  ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+  ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
+  ball.style.transform = `scale(${Math.random()})`;
+  ball.style.width = `${Math.random()}em`;
+  ball.style.height = ball.style.width;
+  
+  balls.push(ball);
+  document.body.append(ball);
+}
+
+// Keyframes
+balls.forEach((el, i, ra) => {
+  let to = {
+    x: Math.random() * (i % 2 === 0 ? -11 : 11),
+    y: Math.random() * 12
+  };
+
+  let anim = el.animate(
+    [
+      { transform: "translate(0, 0)" },
+      { transform: `translate(${to.x}rem, ${to.y}rem)` }
+    ],
+    {
+      duration: (Math.random() + 1) * 2000, // random duration
+      direction: "alternate",
+      fill: "both",
+      iterations: Infinity,
+      easing: "ease-in-out"
+    }
+  );
 });
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Card Using CSS3 , Html and JavaScript
+
+![alt text](https://github.com/th-rpy/card-css-html/blob/main/Demo.gif?raw=true)
